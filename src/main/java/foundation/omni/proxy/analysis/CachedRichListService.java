01295a2ea24f55fd318f292cf23206ed281ded85
Sean Gilligan <sean@msgilligan.com>
2021-03-12 22:14:42 -0800

    use OmniJ 0.5.16, bump btcProxydVersion to 0.1.7
    Move more of build configuration (e.g. dependency versions 
    and useMavenLocal flag!) into gradle.properties
    
    Comment out annotations on CachedRichListService because they were causing
    it to load twice.

--- src/main/java/foundation/omni/proxy/analysis/CachedRichListService.java
+++ src/main/java/foundation/omni/proxy/analysis/CachedRichListService.java
@@ -22,8 +22,8 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Caching wrapper for RichListService
  */
-@Singleton
-@Requires(property="omniproxyd.enabled", value = "true")
+//@Singleton
+//@Requires(property="omniproxyd.enabled", value = "true")
 public class CachedRichListService<N extends Number & Comparable<? super N>, ID> implements RichListService<N, ID> {
     private static final Logger log = LoggerFactory.getLogger(CachedRichListService.class);
     private final RichListService<N, ID> uncachedService;
